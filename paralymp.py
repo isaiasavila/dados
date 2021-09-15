9728b89330ce321f39c91b88d10f0acb0ef3ce3f
Ricardofmc90 <83792362+Ricardofmc90@users.noreply.github.com>
2021-09-14 21:04:30 -0300

    Update paralymp.py

--- paralymp.py
+++ paralymp.py
@@ -1,4 +1,5 @@
 from pyspark.sql.session import SparkSession
+from pyspark.sql.functions import explode_outer
 
 from pyspark.sql.types import *
 import pyspark.sql.functions as F
@@ -11,46 +12,33 @@ spark = SparkSession.builder.appName('firstSeesion')\
 
 path = 'C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/paralympics_tokyo.json'
 df_json = spark.read.option("multiline","true").json(path)
-# df_json.show(10)
-# df_json.printSchema()
-# df_json.select('sex').count() #não retorna nada, converti para Pandas:
-#converter para Dataframe do pandas
-pandasDf = df_json.toPandas()
-print(f'A tabela possui {pandasDf.shape[0]} linhas e {pandasDf.shape[1]} colunas!\n')
-
+# df_json.groupby('age').count().show()
+# x = df_json.select('age').count()
+# print(x) #4525
 
 path = 'C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/Paralympics_tokyo_21.csv'
 df_csv = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
-# df_csv.show()
-# df_csv.printSchema()
-df_csv.select('rank').count()
+# df_csv.groupby('medal').count().show()
+# y = df_csv.select('age').count()
+# print(y) #7917
+df_csv = df_csv.groupby('rank', 'medal').count()
 
-#=================================NÚMERO DE LINHAS DIFERENTES ENTRE O CSV E O JSON, JOIN INPOSSÍVEL==========================
 
-print(pandasDf['rank'].value_counts()) #927 valores únicos, muitos arrays
+df_ex = df_json.select('*', explode_outer(df_json.rank).alias('Ranking'))
+# df_ex.show()
+# df_ex.printSchema()
+# print(type(df_ex)) #<class 'pyspark.sql.dataframe.DataFrame'>
 
-# df_json = df.drop('name')
-# df_csv = df_csv.select('medal', 'name', 'rank')
 
-# df_json.show()
-# df_csv.show()
-
-# df_join = df_json.join(df_csv,df_json['rank'] ==  df_csv['rank'])
+df_join = df_ex.join(df_csv, df_ex.Ranking == df_csv.rank, 'inner') 
 # df_join.show(20)
+# print(type(df_join))#<class 'pyspark.sql.dataframe.DataFrame'>
+# z = df_join.select('age').count()
+# print(z) #7919
 
-# df_join = df_join.toDF(*['Age', 'Nacionalidade', 'Sexo', 'Esporte', 'Seleção', 'Medalha', 'Nome'])
-# df_join.printSchema()
 
-# def colunaTipo(dataframe, nomes, novoTipo):
-#     for nome in nomes:
-#         dataframe = dataframe.withColumn(nome, dataframe[nome].cast(novoTipo))
-#     return dataframe
+df_join = df_join.toDF(*['Idade', 'Categoria', 'Nome', 'Nac', 'rank', 'Gênero', 'Esporte', 'Seleção', 'Ranking', 'rank1'
+                        , 'Medalha', 'count'])
 
-# colunaI = ['Age']
-
-# df_join = colunaTipo(df_join, colunaI, IntegerType())
-
-# df_join.show(10)
-# df_join.printSchema()
-
-# df_join = df_join.groupby('E')
+df_join = df_join.drop('rank', 'rank1', 'count')
+df_join.show()
