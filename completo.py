07339ac55543f699ada508f6ef5dafddef0e20dc
Ricardofmc90 <83792362+Ricardofmc90@users.noreply.github.com>
2021-09-16 08:11:35 -0300

    Update completo.py

--- completo.py
+++ completo.py
@@ -1,9 +1,7 @@
 from pyspark.sql.session import SparkSession
 from pyspark.sql.functions import explode_outer
-
 from pyspark.sql.types import *
 import pyspark.sql.functions as F
-
 
 spark = SparkSession.builder.appName('firstSeesion')\
     .config("spark.master","local[4]") \
@@ -11,54 +9,50 @@ spark = SparkSession.builder.appName('firstSeesion')\
     .config("spark.shuffle.sql.partitions",2 ) \
     .getOrCreate()
 
-path = 'C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/paralympics_tokyo.json'
-df_json = spark.read.option("multiline","true").json(path)
-# df_json.groupby('age').count().show()
-# x = df_json.select('age').count()
+path = 'C:/script/paralympics_tokyo.json'
+df_para_json = spark.read.option("multiline","true").json(path)
+# df_para_json.groupby('age').count().show()
+# x = df_para_json.select('age').count()
 # print(x) #4525
 
-path = 'C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/Paralympics_tokyo_21.csv'
-df_csv = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
-# df_csv.groupby('medal').count().show()
-# y = df_csv.select('age').count()
+path = 'C:/script/Paralympics_tokyo_21.csv'
+df_para_csv = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
+# df_para_csv.groupby('medal').count().show()
+# y = df_para_csv.select('age').count()
 # print(y) #7917
-df_csv = df_csv.groupby('rank', 'medal').count()
+df_para_csv = df_para_csv.groupby('rank', 'medal').count()
 
-
-df_ex = df_json.select('*', explode_outer(df_json.rank).alias('Ranking'))
+df_explode = df_para_json.select('*', explode_outer(df_para_json.rank).alias('Ranking'))
 # df_ex.show()
 # df_ex.printSchema()
 # print(type(df_ex)) #<class 'pyspark.sql.dataframe.DataFrame'>
 
-
-df_join = df_ex.join(df_csv, df_ex.Ranking == df_csv.rank, 'left') 
-# df_join.show(20)
-# print(type(df_join))#<class 'pyspark.sql.dataframe.DataFrame'>
-# z = df_join.select('age').count()
+df_para = df_explode.join(df_para_csv, df_explode.Ranking == df_para_csv.rank, 'left') 
+# df_para.show(20)
+# print(type(df_para))#<class 'pyspark.sql.dataframe.DataFrame'>
+# z = df_para.select('age').count()
 # print(z) #7917
 
-
-df_join = df_join.toDF(*['Idade', 'Categoria', 'Nome', 'Nac', 'rank', 'Gênero', 'Esporte', 'Seleção', 'Ranking', 'rank1'
+df_para = df_para.toDF(*['Idade', 'Categoria', 'Nome', 'Nac', 'rank', 'Gênero', 'Esporte', 'Seleção', 'Ranking', 'rank1'
                         , 'Medalha', 'count'])
 
-df_join = df_join.drop('rank', 'rank1', 'count')
-# df_join.show()
+df_para = df_para.drop('rank', 'rank1', 'count')
+# df_para.show()
 
-path = "C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/population_csv.csv"
+path = "C:/script/population_csv.csv"
 
-df_w2 = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
-df_w2 = df_w2.select(['Country Name','Year','Value']).filter(df_w2['Year'] == '2018')
+df_pop = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
+df_pop = df_pop.select(['Country Name','Year','Value']).filter(df_pop['Year'] == '2018')
 
-df_join_w2 = df_join.join(df_w2, df_join.Seleção == df_w2['Country Name'], 'left')
-# df_join_w2.show()
+df_para_pop = df_para.join(df_pop, df_para.Seleção == df_pop['Country Name'], 'left')
+# df_para_pop.show()
 
-df_join_w2 = df_join_w2.toDF(*['Idade', 'Evento', 'Atleta', 'Nac', 'Gênero', 'Modalidade', 'País', 'Ranking', 'Medalha'
+df_para_pop = df_para_pop.toDF(*['Idade', 'Evento', 'Atleta', 'Nac', 'Gênero', 'Modalidade', 'País', 'Ranking', 'Medalha'
                         , 'country', 'year', 'População_Total'])
 
-df_join_w2 = df_join_w2.select('Atleta', 'Idade', 'Gênero', 'Modalidade', 'Medalha', 'País', 'População_Total')
-# df_join_w2.show()
-# df_join_w2.printSchema()
-
+df_para_pop = df_para_pop.select('Atleta', 'Idade', 'Gênero', 'Modalidade', 'Medalha', 'País', 'População_Total')
+# df_para_pop.show()
+# df_para_pop.printSchema()
 
 def colunaTipo(dataframe, nomes, novoTipo):
     for nome in nomes:
@@ -67,43 +61,27 @@ def colunaTipo(dataframe, nomes, novoTipo):
 
 colunasI = ['Idade', 'População_Total']
 
-df_join_w2 = colunaTipo(df_join_w2, colunasI, IntegerType())
+df_para_pop = colunaTipo(df_para_pop, colunasI, IntegerType())
 
-# df_join_w2.printSchema()
+# df_para_pop.printSchema()
 
-# ct = df_join_w2.select('Country Name').count()
+# ct = df_para_pop.select('País').count()
 # print(ct) #7917
 
 # ===================================================== TRANSFORM ======================================================
-pandasDF = df_join.toPandas()
+# pandasDF = df_join.toPandas()
 # print(type(pandasDF)) # <class 'pandas.core.frame.DataFrame'>
 
-df1 = pandasDF[pandasDF['Medalha'] != 'NA']
-df1 = df1['Medalha'].value_counts()
+# df1 = pandasDF[pandasDF['Medalha'] != 'NA']
+# df1 = df1['Medalha'].value_counts()
 # print(df1)
 
-
-df_join.registerTempTable('temp')
-df_sql = spark.sql('SELECT Medalha, Idade, Esporte FROM temp WHERE Medalha != "NA" ORDER BY Idade')
+#==========================================================SQL==========================================================
+# df_join.registerTempTable('temp')
+# df_sql = spark.sql('SELECT Medalha, Idade, Esporte FROM temp WHERE Medalha != "NA" ORDER BY Idade')
 # df_sql = spark.sql(')
-df_sql.show(1)
-df_sql = spark.sql('SELECT Medalha, Idade, Esporte FROM temp WHERE Medalha != "NA" ORDER BY Idade DESC')
-df_sql.show(1)
+# df_sql.show(1)
+# df_sql = spark.sql('SELECT Medalha, Idade, Esporte FROM temp WHERE Medalha != "NA" ORDER BY Idade DESC')
+# df_sql.show(1)
 
-# df_join1 = df_join.groupby('Idade', 'Medalha').count().sort('Idade').when('Medalha' != 'NA')
-# df_join1.show()
-
-
-# path = "C:/Users/Ricardo Felipe/Desktop/SoulCode/Engenharia de Dados/Projeto FInal/2021_population.csv"
-# df_w = spark.read.load(path, format = 'csv', sep = ',', inferschema = 'true', header = 'true')
-# df_w = df_w.select(['country','world_%','2021_last_updated'])
-# df_w.show()
-# ca = df_w.select('country').count()
-# print(ca) #228
-
-# df_join_w = df_join.join(df_w, df_join.Seleção == df_w.country, 'left') 
-# df_join_w.show()
-# cz = df_join_w.select('country').count()
-# print(cz) #5678
-# zc = df_join_w.select('Idade').count()
-# print(zc) #5678
+#==========================================================SQL========================================================
